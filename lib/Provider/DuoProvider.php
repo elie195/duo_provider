71ee3f6a78d4194225263233966d9c332bc4355f
elie195 <elie195@gmail.com>
2018-01-18 15:06:04 -0800

    Added nginx support. Fixes #18 Now defines getallheaders if backend PHP server doesn't define it

--- lib/Provider/DuoProvider.php
+++ lib/Provider/DuoProvider.php
@@ -39,6 +39,26 @@ class DuoProvider implements IProvider2 {
 	 */
         public function __construct(DuoService $duoService) {
 		$this->duoService = $duoService;
+                /**
+                * 
+                * Define getallheaders for nginx (issue 18) 
+                *
+                */
+                if (!function_exists('getallheaders'))
+                { 
+                    function getallheaders() 
+                    { 
+                        $headers = []; 
+                        foreach ($_SERVER as $name => $value) 
+                        { 
+                            if (substr($name, 0, 5) == 'HTTP_') 
+                            { 
+                                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value; 
+                            } 
+                        } 
+                        return $headers; 
+                    } 
+                } 
         }
 
 
--- templates/admin.php
+++ templates/admin.php
@@ -7,19 +7,21 @@ style('duo', 'style');
   <div class="section">
     <h2 class="app-name">Duo MFA</h2>
     <div class="box" id="duo-settings">
+      <input class="fake" id="fake-username" type="text" name="username">
+      <input class="fake" id="fake-password" type="password" name="password">
       <p><label for="ikey-input">IKEY: </label>
-      <input class="duo-text indent" id="ikey-input" type="text" value="<?php p($_['ikey'])?>" placeholder="DIXXXXXXXXXXXXXXXXXX" autocomplete="off"></p>
+      <input class="duo-text indent" id="ikey-input" type="text" value="<?php p($_['ikey'])?>" placeholder="DIXXXXXXXXXXXXXXXXXX" autocomplete="off" /></p>
       <p><label for="skey-input">SKEY: </label>
-      <input class="duo-text indent" id="skey-input" type="text" value="<?php p($_['skey'])?>" placeholder="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" autocomplete="off"></p>
+      <input class="duo-text indent" id="skey-input" type="text" value="<?php p($_['skey'])?>" placeholder="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" autocomplete="off" /></p>
       <p><label for="host-input">Hostname: </label>
-      <input class="duo-text indent" id="host-input" type="text" value="<?php p($_['host'])?>" placeholder="api-XXXXXXXX.duosecurity.com" autocomplete="off"></p>
+      <input class="duo-text indent" id="host-input" type="text" value="<?php p($_['host'])?>" placeholder="api-XXXXXXXX.duosecurity.com" autocomplete="off" /></p>
       <p><label for="akey-input">AKEY: </label>
-      <input class="duo-text indent" id="akey-input" type="text" value="<?php p($_['akey'])?>" autocomplete="off"><input type="button" id="gen-btn" title="Generate" value="Generate"></p>
+      <input class="duo-text indent" id="akey-input" type="text" value="<?php p($_['akey'])?>" autocomplete="off" /><input type="button" id="gen-btn" title="Generate" value="Generate" /></p>
       <hr>
       <div id="advcd-div">
-      <p><label for="ip-bypass-list">IP Bypass List  <small>(one <strong>IP</strong> per line):</label>
+      <p><label for="ip-bypass-list">IP Bypass List  <small>(one <strong>IP</strong> or <strong>Network (CIDR)</strong> per line):</label>
       <!-- Ugly like this due to spaces when elements are separated with new lines -->
-      <textarea id="ip-bypass-list" rows="7"><?php foreach(explode(',', $_['ipList']) as $value): ?><?php p($value . "\r\n")?><?php endforeach; ?><?php foreach(explode(',', $_['networkList']) as $value): ?><?php p($value . "\r\n")?><?php endforeach; ?></textarea>
+      <textarea id="ip-bypass-list" rows="7" placeholder="192.168.0.0/24"><?php foreach(explode(',', $_['ipList']) as $value): ?><?php p($value . "\r\n")?><?php endforeach; ?><?php foreach(explode(',', $_['networkList']) as $value): ?><?php p($value . "\r\n")?><?php endforeach; ?></textarea>
       <input id="ip-bypass-checkbox" class="checkbox indent" type="checkbox" <?php p($_['ipEnabled']==true ? 'checked' : '')?>>
       <label for="ip-bypass-checkbox"> IP Bypass Enabled?  <small>(advanced)</small></label></p>
       <input id="ldap-bypass-checkbox" class="checkbox indent" type="checkbox" <?php p($_['ldapEnabled']==true ? 'checked' : '')?>>
