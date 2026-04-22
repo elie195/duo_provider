880de0f21fbc3051327d3408d69c3d9c29fd2003
elie <2161018+elie195@users.noreply.github.com>
2026-04-22 15:39:11 -0700

    Remove CI debugging

--- admin.php
+++ admin.php
@@ -17,6 +17,7 @@
  * You should have received a copy of the GNU Affero General Public License, version 3,
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  * 
+ * Dummy change to trigger CI
  *
  */
 
