9745304f7581d7b4041037ed64337398ef0f05f7
elie <2161018+elie195@users.noreply.github.com>
2026-04-22 15:07:04 -0700

    More CI debugging

--- admin.php
+++ admin.php
@@ -17,7 +17,6 @@
  * You should have received a copy of the GNU Affero General Public License, version 3,
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  * 
- * Dummy change to trigger CI
  *
  */
 
